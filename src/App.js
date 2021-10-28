449a9007b1cafabe2720693a3e10d3a05f06b357
vectortracey <miek1128@hotmail.com>
2021-10-28 17:01:24 +0100

    Updated with travis and testing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-quill/dist/quill.snow.css";
 import "./App.css";
 
-import AddDocument from "./components/add-document.component.js";
-import Document from "./components/document.component.js";
-import DocumentsList from "./components/document-list.component.js";
+import CreateDocument from "./components/document/create.component.js";
+import ViewDocument from "./components/document/view.component.js";
+import ListDocument from "./components/document/list.component.js";
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/documents" className="navbar-brand">
-            Kmom02 react
+            Kmom03 react
           </a>
+
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
               <Link to={"/documents"} className="nav-link">
@@ -32,12 +35,13 @@ class App extends Component {
 
         <div className="container mt-3">
           <Switch>
-            <Route exact path={["/", "/documents"]} component={DocumentsList} />
-            <Route exact path="/add" component={AddDocument} />
-            <Route path="/documents/:id" component={Document} />
+            <Route exact path={["/", "/documents"]} component={ListDocument} />
+            <Route exact path="/add" component={CreateDocument} />
+            <Route path="/documents/:id" component={ViewDocument} />
           </Switch>
         </div>
       </div>
+   </Router>
     );
   }
 }
--- src/http-common.js
+++ src/http-common.js
@@ -4,5 +4,6 @@ export default axios.create({
   baseURL: "https://jsramverk-editor-miek19.azurewebsites.net/api",
   headers: {
     "Content-type": "application/json",
+    
   },
 });
