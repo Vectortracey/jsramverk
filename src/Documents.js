300b7e5f18a9bd802d1d399f47c19e1b627b3329
vectortracey <miek1128@hotmail.com>
2022-12-09 16:08:59 +0100

    kmom05 auth frontend

--- src/Documents.js
+++ src/Documents.js
@@ -1,21 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-export default function Documents({ onTitleClick }) {
-    const [data, setData] = useState({})
+export default function Documents({ currToken, onTitleClick }) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch("http://localhost:1337/documents", {
+      headers: {
+        "x-access-token": currToken,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
 
-    useEffect(() => {
-        fetch("http://localhost:1337/documents")
-        .then(res => res.json())
-        .then(data => setData(data))
-    }, [])
-
+  if (data.length > 0) {
     let allDocs = [];
 
     for (let i = 0; i < data.length; i++) {
-        allDocs.push(<div className="indDoc" onClick={() => onTitleClick(data[i]._id, data[i].name, data[i].text)}>{data[i].name}</div>);
+      allDocs.push(
+        <div
+          className="indDoc"
+          onClick={() => onTitleClick(data[i]._id, data[i].name, data[i].text)}
+        >
+          {data[i].name}
+        </div>
+      );
     }
 
-    return (
-        allDocs
-    )
+    return allDocs;
+  }
+  return "Skapa ett dokument för att se det här!";
 }
--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,32 +1,40 @@
-import { React, useRef } from 'react';
+import React from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import Toolbar from './Toolbar.js';
 import Documents from './Documents.js';
+import Auth from './Auth.js';
 
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://localhost:1337";
+const ENDPOINT = "http://localhost:1337/";
 
 const socket = socketIOClient(ENDPOINT);
 
 
-export default function TextEditor() {
-  const editorRef = useRef(null);
-  const log = () => {
-    if (editorRef.current) {
-      let text = editorRef.current.getContent();
+export default class TextEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.editorRef = React.createRef();
+    this.state = {
+        token: null,
+        email: null
+    };
+  }
+  log = () => {
+    if (this.editorRef.current) {
+      let text = this.editorRef.current.getContent();
       console.log(text);
     }
   };
-  const empty = () => {
-    if (editorRef.current) {
+  empty = () => {
+    if (this.editorRef.current) {
       const inputId = document.getElementById('idDoc');
       inputId.value = "";
       const inputName = document.getElementById('nameDoc');
       inputName.value = "";
-      editorRef.current.setContent("");
+      this.editorRef.current.setContent("");
     }
   };
-  const save = (e) => {
+  save = (e) => {
       if (e.target[1].value) {
           e.preventDefault();
           fetch(`http://localhost:1337/documents/${e.target[1].value}`, {
@@ -34,25 +42,37 @@ export default function TextEditor() {
               headers: {
                   'Accept': 'application/json',
                   'Content-Type': 'application/json',
+                  'x-access-token': this.state.token
               },
               body: JSON.stringify({
+                  id: e.target[1].value,
                   name: e.target[0].value,
-                  text: editorRef.current.getContent(),
+                  text: this.editorRef.current.getContent(),
+                  owner: this.state.email,
+                  allowed_users: [
+                    e.target[2].value
+                  ]
               })
           });
           alert("Uppdaterad!");
       }
       else {
+        console.log(e.target[2].value);
           e.preventDefault();
           fetch('http://localhost:1337/documents', {
               method: 'POST',
               headers: {
                   'Accept': 'application/json',
                   'Content-Type': 'application/json',
+                  'x-access-token': this.state.token
               },
               body: JSON.stringify({
                   name: e.target[0].value,
-                  text: editorRef.current.getContent(),
+                  text: this.editorRef.current.getContent(),
+                  owner: this.state.email,
+                  allowed_users: [
+                    e.target[2].value,
+                  ],
               })
           })
           .then( function (response) {
@@ -60,53 +80,115 @@ export default function TextEditor() {
           });
       };
   };
-  function showText(docId, name, text) {
-      const inputId = document.getElementById('idDoc');
-      inputId.value = docId;
-      const inputName = document.getElementById('nameDoc');
-      inputName.value = name;
-      editorRef.current.setContent(text);
-      socket.emit("create", docId);
-  };
-  const live = () => {
+  showText = (docId, name, text) => {
+        const inputId = document.getElementById('idDoc');
+        inputId.value = docId;
+        const inputName = document.getElementById('nameDoc');
+        inputName.value = name;
+        this.editorRef.current.setContent(text);
+        socket.emit("create", docId);
+    };
+  live = () => {
     socket.on("doc", (data) => {
-      editorRef.current.setContent(data.html);
+      this.editorRef.current.setContent(data.html);
     });
 
     const inputId = document.getElementById('idDoc');
     let data = {
         _id: inputId.value,
-        html: editorRef.current.getContent()
+        html: this.editorRef.current.getContent()
     };
     socket.emit("doc", data);
   };
-  return (
-    <>
-    < Toolbar saveText={save} logText={log} emptyEditor={empty} />
-      <Editor
-        id="myTextarea"
-        onInit={(evt, editor) => editorRef.current = editor}
-        onKeyUp={live}
-        initialValue=""
-        init={{
-          height: 300,
-          menubar: false,
-          plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount'
-          ],
-          toolbar: 'undo redo | formatselect | ' +
-          'bold italic backcolor | alignleft aligncenter ' +
-          'alignright alignjustify | bullist numlist outdent indent | ' +
-          'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
-      />
-      <div className="documents">
-      <h3>Välj ett dokument att redigera:</h3>
-        < Documents onTitleClick={showText} />
-      </div>
-    </>
-  );
+
+  login = (e) => {
+    let that = this;
+    e.preventDefault();
+          fetch('http://localhost:1337/login', {
+              method: 'POST',
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                  email: e.target[0].value,
+                  password: e.target[1].value,
+              })
+          })
+          .then( function (response) {
+            return response.json();
+
+          }).then(function(data) {
+            if (data.errors) {
+              if (data.errors.title === "Wrong password") {
+                alert("Fel lösenord, försök igen.")
+              } else if (data.errors.title === "User not found") {
+              alert("Denna användare finns inte. Registrera dig för att logga in.")
+              }
+            } else if (data.data) {
+              that.setState({token: data.data.token, email: data.data.user.email});
+            }
+          })
+  };
+  reg = (e) => {
+    e.preventDefault();
+          fetch('http://localhost:1337/register', {
+              method: 'POST',
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                  email: e.target[0].value,
+                  password: e.target[1].value,
+              })
+          })
+          .then( function (response) {
+            return response.json();
+
+          }).then(function(data) {
+            if (data.errors) {
+              if (data.errors.title === "Email or password missing") {
+                alert("Mejladress eller lösenord saknas.")
+              }
+            } else if (data.data.message === "User successfully registered.") {
+              alert("Ditt konto är registrerat!")
+            }
+          })
+  };
+  render() {
+    if (this.state.token == null) {
+      return <Auth loginUser={this.login} registerUser={this.reg} />
+    }
+    return (
+      <>
+      < Toolbar saveText={this.save} logText={this.log} emptyEditor={this.empty} />
+      <p className="currUser">Inloggad användare: { this.state.email }</p>
+        <Editor
+          id="myTextarea"
+          onInit={(evt, editor) => this.editorRef.current = editor}
+          onKeyUp={this.live}
+          initialValue=""
+          init={{
+            height: 300,
+            menubar: false,
+            plugins: [
+              'advlist autolink lists link image charmap print preview anchor',
+              'searchreplace visualblocks code fullscreen',
+              'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar: 'undo redo | formatselect | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+          }}
+        />
+        <div className="documents">
+        <h3>Välj ett dokument att redigera:</h3>
+          < Documents  onTitleClick={this.showText} currToken={this.state.token}/>
+        </div>
+      </>
+    );
+  }
 }
